74df9ef23118be8029a5cc0f54e16dff03879b96
Ajay Dhawan <ajay.dhawan99@gmail.com>
2020-12-09 15:16:26 +0530

    1st commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDOrJtlLOwTIyOYxNtPFCOQkIqo1JILfwY",
+    authDomain: "insta-clone-204ce.firebaseapp.com",
+    databaseURL: "https://insta-clone-204ce-default-rtdb.firebaseio.com",
+    projectId: "insta-clone-204ce",
+    storageBucket: "insta-clone-204ce.appspot.com",
+    messagingSenderId: "223990357728",
+    appId: "1:223990357728:web:15a083855aa304d0a73a85"
+  },
 };
 
 /*
