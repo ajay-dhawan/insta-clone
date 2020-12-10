4e70fa1bcbab12650f837eef4930f9d05791a60c
ajay-dhawan <ajay.dhawan1705@gmail.com>
2020-12-10 20:07:01 +0530

    production env firebase added to env-prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDOrJtlLOwTIyOYxNtPFCOQkIqo1JILfwY",
+    authDomain: "insta-clone-204ce.firebaseapp.com",
+    databaseURL: "https://insta-clone-204ce-default-rtdb.firebaseio.com",
+    projectId: "insta-clone-204ce",
+    storageBucket: "insta-clone-204ce.appspot.com",
+    messagingSenderId: "223990357728",
+    appId: "1:223990357728:web:15a083855aa304d0a73a85"
+  },
 };
